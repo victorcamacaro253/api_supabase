9b2dd252a7aefc1877521cf52cfca15ae805602e
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-10-30 00:15:01 -0400

    Add files via upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -134,10 +134,11 @@ if(existingUser){
    return res.status(400).json({ error: 'El usuario ya existe' });
 }
 
-   
+   const hashedpassword=  await hash(password, 10)
 
 
-   const result= await User.addUser(name,apellido,cedula,email,password)
+
+   const result= await User.addUser(name,apellido,cedula,email,hashedpassword)
 
    if(!result){
       return res.status(400).json({message:'Error al agregar usuario'})
@@ -211,7 +212,7 @@ if(existingUser){
     if (name) updateFields.nombre = name;
     if (apellido) updateFields.apellido = apellido;
     if (cedula) updateFields.cedula = cedula;
-    if (correo) updateFields.correo = correo;
+    if (email) updateFields.email = email;
 
 
      const result = await User.updateUser(id,updateFields);
@@ -334,9 +335,134 @@ static login =async(req,res)=>{
     
 }
 
+//----------------------------------------------------------------
+ static getUserByCedula= async (req,res)=>{
+   const {cedula}= req.query
+
+   
+  const errors= validationResult(req)
+  if (!errors.isEmpty()) {
+   return res.status(400).json({errors:errors.array()})
+   
+  }
+
+  try {
+   const user= await User.getUserByCedula(cedula)
+   if(!user){
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+      
+   }
+
+   res.status(200).json(user)
+   
+  } catch (error) {
+   console.log(error)
+   return res.status(500).json({message: error.message})
+
+  }
+
+ }
+
+
+ //----------------------------------------------------------------------
+
+ static getLoginHistory= async  (req,res)=>{
+   const {id}= req.params
+   const errors= validationResult(req)
+   if (!errors.isEmpty()) {
+      return res.status(400).json({errors:errors.array()})
+      }
+
+      try {
+
+         const loginHistory= await User.getLoginHistory(id)
+
+         const meta={
+            Numeroingresos:  loginHistory.length,
+            Ultimoingreso: loginHistory[loginHistory.length-1].fecha,
+            historial: loginHistory
+
+         }
+
+         if(!loginHistory){
+            return res.status(404).json({ message: 'Historial de login no encontrado' });
+         }
+
+         res.status(200).json(meta)
+
+         
+      } catch (error) {
+         console.log(error)
+         return res.status(500).json({message: error.message})
+
+      }
+
+
+}
+
+//-----------------------------------------------------------------------------------------------
+ static getUsersWithPagination = async (req,res)=>{
+   const{ page=1,limit=10}= req.query
+   const offset= (page-1) * limit;
+
+   try {
+      const users = await User.getUsersWithPagination(limit, offset);
+      res.status(200).json(users)
+
+      
+   } catch (error) {
+      console.log(error)
+      return res.status(500).json({message: error.message})
+      
+   }
+ }
+
+
+
+//-------------------------------------------------------------------------------------------
+
+static searchUsers= async  (req,res)=>{
+   const {name,apellido,cedula, email}= req.query
+   try {
+
+
+      const fields={}
+
+       // Preparar los campos a actualizar
+    if (name) fields.nombre = name;
+    if (apellido) fields.apellido = apellido;
+    if (cedula) fields.cedula = cedula;
+    if (email) fields.email = email;
+
+    // Crea una clave única para la consulta
+    const cacheKey = JSON.stringify(fields);
+
+    // Intenta obtener los datos de Redis
+    const cachedUsers = await redis.get(cacheKey);
+    if (cachedUsers) {
+        // Si hay datos en caché, devuélvelos
+        console.log('Usuario obtenido mediante redis')
+
+        return res.status(200).json(JSON.parse(cachedUsers));
+    }
+
+      const users = await User.searchUsers(fields);
+
+     // Almacena los resultados en Redis por un tiempo determinado (ej. 1 hora)
+     await redis.set(cacheKey, JSON.stringify(users), 'EX', 3600);
+
+      res.status(200).json(users)
+
+   }catch(error){
+      console.log(error)
+      return res.status(500).json({message: error.message})
+   }
+
+
 }
 
 
+}
 
 
 export default userController
