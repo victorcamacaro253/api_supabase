d2a68167ea06e734e677707c86381da7d565eac6
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-10-31 00:22:27 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 import express,{json} from "express";
 import userRoutes from  "./routes/userRoutes.js";
-
+import cors from 'cors'
+import helmet from "helmet";
 
 const app= express();
+
+app.disable('x-powered-by')
+
 
 app.get('/',(req,res)=>{
     res.send('Hola')
 })
 app.use(json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cors())
+
+app.use(helmet())
 
 app.use('/users',userRoutes)
 
 
-app.disable('x-powered-by')
 
 const PORT = process.env.PORT || 3004;
 
