411cee00b2dbbbcc4f2260dc1f06090d930bb232
Victor C <victorcamacaro253@gmail.com>
2024-11-10 21:35:06 -0400

    Add files via upload

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,48 +1,84 @@
-import { Router } from 'express';
-import productController from '../controllers/productController.js';
-import upload from '../middleware/multerConfig.js';
-
-const router = Router();
-
-//Ruta para obtener los productos filtrado por nombre
-router.get('/searchProductByName',productController.searchProductByName)
-
-//Ruta para obtener todos los productos
-router.get('/',productController.getProducts)
-
-//Ruta para obtener los porductos por categoria
-router.get('/categoria/',productController.getProductsByCategoria)
-
-//Ruta para obtener los productos por un rango de precio
-router.get('/price',productController.getProductsByPrinceRange)
+import { Router } from "express";
+import productsController from "../controllers/productsController.js";
+import {body,check,param,query,validationResult} from  "express-validator";
+import upload from "../middleware/multerConfig.js";
 
 
-//Ruta para obtener los productos por id
-router.get('/:id',productController.getProductsById)
+const router= Router();
 
-//Ruta para ingresar multiples productos
-router.post('/addMultipleProducts',upload.single('image'),productController.addMultipleProducts)
+router.get('/',productsController.getProducts)
 
+router.get('/name',
+    query('name').notEmpty().withMessage('El producto es obligatorio'),
+    productsController.getProductByName)
 
-//Ruta para añadir un producto
+    
 
-router.post('/',upload.single('image'),productController.addProduct);
-
-
-//Ruta para actualizar un producto
-
-router.put('/:id',productController.updateProduct)
-
-//Ruta para eliminar multiples productos
-router.post('/deleteMultipleProducts',productController.deleteMultipleProducts)
+     //Ruta para obtener los productos por un rango de precio
+     router.get('/price',
+        query('min').isDecimal().optional().withMessage('El precio minimo es requerido'),
+        query('max').isDecimal().optional().withMessage('El precio maximo es requerido'),
+        productsController.getProductByPriceRange)
 
 
+ //Ruta para obtener los productos mas vendidos
 
-//Ruta para eliminar el producto
-
-router.delete('/:id',productController.deleteProduct)
+router.get('/topSelling',productsController.getTopSelling)
 
 
+router.get('/:id',
+          param('id').isInt().withMessage('El ID debe ser un numero entero'),
+          productsController.getProductsById)
+
+
+    
+router.post('/', 
+    body('id_proveedor').isInt().withMessage('El id_proveedor debe ser un numero entero'),
+    body('nombre_producto').notEmpty().withMessage('El producto es obligatorio'),
+    body('precio').isFloat().withMessage('El precio debe ser un numero float'),
+    body('descripcion').notEmpty().withMessage('La descripción debee teef'),
+    body('stock').isInt().withMessage('El stock debe ser un numero entero'),
+    body('id_categoria').isInt().withMessage('El id_categoria debe ser un numero entero'),
+   // upload.single('imagen'),
+     productsController.createProduct)
+
+
+router.put('/:id',
+    [param('id').isInt().withMessage('El ID debe ser un número entero'),
+    body('id_proveedor').optional().isInt().withMessage('El id_proveedor debe ser un numero entero'),
+    body('nombre_producto').notEmpty().withMessage('El producto es obligatorio'),
+    body('precio').optional().isFloat().withMessage('El precio debe ser un numero float'),
+    body('descripcion').optional().notEmpty().withMessage('La descripción debee teef'),
+    body('stock').optional().isInt().withMessage('El stock debe ser un numero entero'),
+    body('id_categoria').optional().isInt().withMessage('El id_categoria debe ser un numero entero')],
+    productsController.updateProduct)
+
+
+
+
+router.delete('/:id',
+      [param('id').isInt().withMessage('El ID debe ser un número entero')],
+     productsController.deleteProduct)
+
+
+
+router.post('/addMultipleProducts',
+    
+    [
+        check('products').isArray().optional().withMessage('Products must be an array'),
+        check('products.*.nombre_producto').notEmpty().withMessage('Nombre del producto es requerido'),
+        check('products.*.descripcion').notEmpty().withMessage('Descripcion del producto es requerido'),
+        check('products.*.precio').notEmpty().withMessage('El precio es requerido'),
+        check('products.*.stock').notEmpty().withMessage('stock del producto es requerido'),
+        check('products.*.id_categoria').notEmpty().withMessage('id de la categoria del producto es requerido'),
+        check('products.*.id_proveedor').notEmpty().withMessage('id del proveedor del producto es requerido'),
+],
+          upload.array('image'),
+          productsController.addMultipleProducts)
+
+
+//Ruta para importar un archivo csv
+router.post('/import',upload.single('file'),productsController.importProduct)
 
 
 export default router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,75 +1,129 @@
-import { Router } from 'express';
-import userController from '../controllers/userController.js';
-import authenticateToken from '../middleware/authenticationToken.js';
-import upload from '../middleware/multerConfig.js';
-import checkPermissions from '../middleware/checkPermission.js';
+import { Router } from "express";
+import userController from "../controllers/userController.js";
+import {body,check,param,query,validationResult} from    "express-validator";
+import validarErrores from "../middleware/validarErrores.js";
+import upload from "../middleware/multerConfig.js";
 
-const router = Router();
-
-//Ruta para obtener los datos de la base de datos
-
-//Ruta para obtener todos los usuarios de la base de datos
-router.get('/',userController.getAllUser);
+const router= Router();
 
 
+
+router.get('/',userController.getUsers)
+
+
+router.get('/name',
+    query('name').notEmpty().withMessage('El nombre es obligatorio'),
+    userController.getUserByName)
+
+    
+ // Ruta para obtener la sesión activa
+  router.get('/session', userController.getSession);
+
+
+  router.get('/dni',  
+    query('cedula').notEmpty().withMessage('La cedula es obligatorio'),
+    userController.getUserByCedula)
+
+    
+  router.get('/page',userController.getUsersWithPagination)
+
+ 
 //Ruta para obtener los usuarios filtrados
-router.get('/searchUser',userController.searchUsers);
-
-//Ruta para obtener el perfi de los usuarios
-router.get('/getperfil',authenticateToken,checkPermissions('read') ,userController.getPerfil)
-
-
-
-//Ruta para obtener el historial de ingresos del usuario por el nombre
-router.get('/loginHistorial',userController.getLoginHistory)
+router.get('/searchUser',[  
+    query('name').optional().notEmpty().withMessage('El nombre es obligatorio'),
+    query('apellido').optional().notEmpty().withMessage('El apellido es obligatorio'),
+    query('cedula').optional().notEmpty().withMessage('La cedula es obligatorio'),
+    query('email').isEmail().withMessage('El email es obligatorio'),
+    
+],userController.searchUsers);
 
 
-
-//Ruta para obtener el perfil del usuario
-router.get('/getUserPerfil/:id',authenticateToken ,userController.getUserPerfil)
-
-
-//Ruta para obtener los usarios con paginacion
-router.get('/pagination',userController.getUsersWithPagination)
+  router.get('/loginHistory/:id',
+        param('id').isInt().withMessage('El id tiene que ser un numero entero'),
+        validarErrores, //midleware para manerjar errores
+        userController.getLoginHistory )
 
 
-//Ruta para agregar un nuevo usuario
-
-router.post('/',authenticateToken,userController.addUser);
-
-//Ruta para logearse
-
-router.post('/login', userController.loginUser);
 
 
-//Ruta para obetenr un usuario por id
+  router.get('/:id',
+    param('id').isInt().withMessage('El ID debe ser un numero entero'),
+    validarErrores, //midleware para manerjar errores
+    userController.getUserById)
 
-router.get('/:id',userController.getUserById);
 
 
-//Ruta para insertar multiples usuarios
-router.post('/addMultipleUsers',userController.addMultipleUsers)
+router.post('/add',
+    [body('name').notEmpty().withMessage('El nombre es obligatorio'),
+    body('apellido').notEmpty().withMessage('El apellido es obligatorio'),
+    body('email').isEmail().withMessage('El email no es valido'),
+    body('password').isLength({min:7}).withMessage('La contraseña debe tener al menos 7 caracteres')],
+    userController.addUser)
 
-//Ruta para eliminar multiple usuarios
-router.post('/deleteMultipleUsers',userController.deleteMultipleUsers)
+router.delete('/:id',
+[param('id').isInt().withMessage('El ID debe ser un número entero')],
+userController.deleteUser)
 
-//Ruta para solicitar el password reset
-router.post('/requestPasswordRequest',userController.requestPasswordReset)
+router.put('/:id',
+    [
+        param('id').isInt().withMessage('El ID debe ser un número entero'),
+        body('name').optional().notEmpty().withMessage('El nombre no puede estar vacío'),
+        body('apellido').optional().notEmpty().withMessage('El apellido no puede estar vacío'),
+        body('email').optional().isEmail().withMessage('Correo no válido'),
+    ]
+    ,userController.updateUser)
 
-//Ruta para resetear el password
-router.post('/resetPassword/:token',userController.resetPassword)
+// Login usando el metodo de autneticacion de supabase
+    router.post('/loginSupabase',[
 
-//Ruta para actualizar un usuario
+        body('email').isEmail().withMessage('El correo no es valido'),
+        body('password').isLength({min:7}).withMessage('La contraseña debe tener al menos 7 caracteres')
+    ],
+    userController.loginSupabase)
 
-router.put('/:id', userController.updateUser);
 
-//Ruta para eliminar un producto
+    router.post('/logoutSupabase',userController.logoutSupabase)
 
-router.delete('/:id', userController.deleteUser);
 
-//Ruta para actualizar un producto parcial
+    //Logins normal usando los datos en la base de datos en supabase
+    router.post('/login',[
 
-router.patch('/:id', userController.partialUpdateUser)
+        body('email').isEmail().withMessage('El correo no es valido'),
+        body('password').isLength({min:7}).withMessage('La contraseña debe tener al menos 7 caracteres')
+    ],
+    userController.login)
+
+
+
+    router.post('/addMultipleUsers',
+        [
+            check('users').isArray().optional().withMessage('Users must be an array'),
+            check('users.*.name').notEmpty().withMessage('Name  es requerido'),
+            check('users.*.apellido').notEmpty().withMessage('Apellido es requerido'),
+            check('users.*.cedula').notEmpty().withMessage('Cedula es requerido'),
+            check('users.*.email').isEmail().withMessage('Invalido formato de email'),
+            check('users.*.password').isLength({ min:7 }).withMessage('Password debe ser al menos 7  caracteres')
+
+        ]
+        ,upload.array('image'),userController.addMultipleUsers)
+
+    router.post('/delete',[
+      
+        body('ids').isArray().withMessage('Ids deb ser un array')
+        .custom((ids)=>{
+            ids.forEach(id => {
+                if(!Number.isInteger(id)){
+                    throw new Error(`El ID es ${id} no es un numero entero valido`)
+                }
+            });
+
+            return true;
+        })
+
+    ],
+    validarErrores,
+    userController.deleteMultipleUsers)
+  
 
 
 export default router;
