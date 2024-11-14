be7fa2699655b38c38e7437f86bbe142792f694a
Victor C <victorcamacaro253@gmail.com>
2024-11-13 23:11:32 -0400

    Add files via upload

--- routes/comprasRoutes.js
+++ routes/comprasRoutes.js
@@ -1,34 +1,8 @@
-import { Router } from 'express';
-import authenticateToken from '../middleware/authenticationToken.js';
-import comprasController from '../controllers/comprasController.js';
+import { Router } from "express";
+import comprasController from "../controllers/comprasController.js";
 
+const router = Router()
 
-const router = Router();
-
-
-//Ruta para obtener el listado de las compras
-router.get('/',comprasController.getCompras);
-
-//Ruta para obtener las compras de un usuario por su nombre
-router.get('/SearchUserCompras/',comprasController.getComprasCountByUsuario)
-
-router.get('/fecha', comprasController.getComprasByDate);
-
-
-router.get('/comprasByUser',comprasController.getComprasCountByUsuario)
-
-//Ruta para obtener las compras de un usuario
-router.get('/:id',comprasController.getComprasByUserId);
-
-
-//Ruta para comprar un producto
-router.post('/',comprasController.compraProduct);
-
-
-
-//Ruta para eliminar una compra
-router.delete('/:id',comprasController.deleteCompra)
-
+router.get('/',comprasController.getCompras)
 
 export default router
-
