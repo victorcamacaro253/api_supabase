e1f1ea4eea6e0ba16cfac8ff27b9ee718443ab99
Victor C <victorcamacaro253@gmail.com>
2024-11-24 23:28:54 -0400

    Add files via upload

--- routes/comprasRoutes.js
+++ routes/comprasRoutes.js
@@ -1,15 +1,35 @@
 import { Router } from "express";
 import comprasController from "../controllers/comprasController.js";
+import {body,check,param,query,validationResult} from  "express-validator";
+
 
 const router = Router()
 
 //Ruta para obtener las compras 
 router.get('/',comprasController.getCompras)
 
+//Ruta para obtener las compras de un usuario por su nombre
+router.get('/comprasUser/',comprasController.getComprasCountByUsuario)
+
+router.get('/user/:id',
+    param('id').isInt().withMessage('El ID debe ser un numero entero'),
+comprasController.getComprasByUserId)
+
+
 //Ruta para obtener una compra por id
-router.get('/:id',comprasController.getCompraById)
+router.get('/:id',
+param('id').isInt().withMessage('El ID debe ser un numero entero'),
+comprasController.getCompraById)
 
 //Ruta para agrear una compra
-router.post('/',comprasController.compraProduct)
+router.post('/',
+    [
+        body('id_usuario').isInt().withMessage('El id del usuario debe ser un numero entero'),
+        body('productos').isArray().withMessage('Los productos deben ser un array').notEmpty().withMessage('Se requiere al menos un producto'),
+        body('productos.*.id_producto').isInt().withMessage('El id producto debe ser un numero entero'),
+        body('productos.*.cantidad').isInt({gt:0}).withMessage('La cantidad debe ser un numero entero mayor a 0'),
+        body('productos.*.precio').isFloat({gt:0}).withMessage('El precio debe ser un numero mayor a 0')
+    ]
+    ,comprasController.compraProduct)
 
 export default router
